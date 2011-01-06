12b9601d1aba74415560cdeee57dd5d833f28167
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-01-05 21:53:24 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tripper::Application.config.secret_token = '4a6e52e87a13add776cbf38079a7bdc6ef07edc74fcd5e50862e1f6ae5ab435302ed84e6220656abc890dd8491b7ae4bf8e85ea406fdc6bc4b224efe7b3b17fa'
