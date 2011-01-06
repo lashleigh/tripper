2673f61f33cbba651df733de3ed2c7088b28e153
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-01-06 03:51:24 -0800

    Starting playing with tagging and a bit of reformatting, it's still just a toy

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,23 @@
 class Place < ActiveRecord::Base
+  acts_as_taggable
   has_many :comments, :dependent => :destroy
   belongs_to :user
+
+  def short_content(maxlen = 100)
+    s = description[0..maxlen]
+    if( s.length > maxlen and s.include? ' ')
+      s[0..s.rindex(' ')-1] + '...'
+    else
+      s
+    end
+  end
+
+  def author
+    if user
+      user.name
+    else
+      "unknown"
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110106101758) do
+ActiveRecord::Schema.define(:version => 20110106103143) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(:version => 20110106101758) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
