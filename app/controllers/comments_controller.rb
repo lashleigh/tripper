5ec0075d4f370bb5948c44ad2672e4eded38c313
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-01-11 01:10:23 -0800

    Comments need to be polymorphized still, or swallowed into something larger like Visit. There are some oddities with deleting throughout the project that are present in comments as well.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
-  before_filter :authenticate, :only => :destroy
+  before_filter :authenticate_user!
 
   def create
     @place = Place.find(params[:place_id])
     @comment = @place.comments.create(params[:comment])
+    @comment.user = current_user
+    @comment.save
     redirect_to place_path(@place)
   end
 
