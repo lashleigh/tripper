f12ac9acf90dd4ceefaa96ef96408be3b8b14512
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-01-17 19:33:19 -0800

    Added comments to the activity page but it still needs a lot of formatting work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
     @places = Place.all.reverse
+    @comments = Comment.all.reverse
+    @activity = (@places + @comments).sort { |a, b| b.created_at <=> a.created_at }
 
     respond_to do |format|
       format.html # index.html.erb
